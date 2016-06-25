ec80576e348ce784aadf311a48dfba42db306c0c
Holman Gao <holman@golmansax.com>
2016-06-24 23:20:31 -0700

    Try to get test to pass

--- src/test/scala/com/golmansax/app/TodoAppServletSpec.scala
+++ src/test/scala/com/golmansax/app/TodoAppServletSpec.scala
@@ -1,14 +1,17 @@
 package com.golmansax.app
 
+import com.mongodb.casbah.Imports._
 import org.scalatra.test.specs2._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
-class TodoAppServletSpec extends ScalatraSpec { def is =
-  "GET / on TodoAppServlet"                     ^
-    "should return status 200"                  ! root200^
-                                                end
+class TodoAppServletSpec extends ScalatraSpec { def is = s2"""
+  GET / on TodoAppServlet
+    should return status 200      $root200
+  """
 
-  addServlet(classOf[TodoAppServlet], "/*")
+  val mongoClient = MongoClient()
+  val mongoColl = mongoClient("todo_app_in_scala")("todos")
+  addServlet(new TodoAppServlet(mongoColl), "/*")
 
   def root200 = get("/") {
     status must_== 200
